94c2f222d4cea102d7a66b3d7ec3833daa9d44c0
ScriptD <adironaldo1234@gmail.com>
2024-04-29 22:26:15 +0530

    update: made tabs responsive for screen<360px

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -6,8 +6,9 @@ interface propType {
 	children: React.ReactNode;
 }
 const Tabs: FC<propType> = ({children}) => {
+	// TODO - make response for se and 5s
 	return (
-		<div className="tabs bg-dark-200/70 border-[1px] border-solid border-white/30 fixed bottom-4 flex justify-between items-center text-center left-1/2 w-[90%] max-w-[400px]  rounded-full -translate-x-1/2 text-sm gap-2 backdrop-blur-md lowercase isolate text-generic-100">
+		<div className="tabs bg-dark-200/70 border-[1px] border-solid border-white/30 fixed bottom-4 flex justify-between items-center text-center left-1/2 w-[90%] max-w-[400px]  rounded-full -translate-x-1/2 text-sm md:gap-2 backdrop-blur-md lowercase isolate text-generic-100">
 			{children}
 		</div>
 	);
